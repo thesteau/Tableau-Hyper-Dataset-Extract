0e8bfd9218c52cc1f18910d53ceb527b28da8dc0
thesteau <25945183+thesteau@users.noreply.github.com>
2021-03-10 22:20:29 -0800

    OOP version of the initial script

--- HyperConvert.py
+++ HyperConvert.py
@@ -1,5 +1,6 @@
 # Script by Steven Au
 # Special thanks to the Pantab author!
+# Converts a Tableau Hyper Data Export to a CSV file.
 import pathlib as pl
 import platform as pf
 
@@ -9,21 +10,20 @@ import pandas as pd
 
 def monologue(func):
     """ Monologue Decorator"""
-
-    def wrapped(self):
-        """"""
+    def wrapped(*args, **kwargs):
+        """ Wrapper per decorator"""
         print("You can choose to drag and drop a file path here!")
         print("Otherwise, if the paths are hard coded into this script, ")
         print("then do not type anything and simply just press enter")
-        func(self)
-
+        func(*args, **kwargs)
     return wrapped
 
+
 class HyperConvert:
-    """"""
+    """ Hyper Dataset conversion object."""
 
     def __init__(self, script_path=None, file_type='csv'):
-        """"""
+        """ Initiator - all variables are optional"""
         self._script_path = script_path
         self._file_type = file_type
 
@@ -36,7 +36,7 @@ class HyperConvert:
         return self._script_path
 
     def set_file_type(self, file_type):
-        """"""
+        """ Manually set the data file as desired."""
         self._file_type = file_type
 
     def library_pathing(self, some_file):
@@ -78,6 +78,9 @@ class HyperConvert:
                 new_export:
                     Export dataset path
         """
+        if self._script_path is None:
+            evaluate_file_path = self._script_path
+
         while True:
             try:
                 dragged_file = input("Press enter to use the file paths hard saved in this script\n> ")
@@ -88,17 +91,15 @@ class HyperConvert:
 
         if len(dragged_file) != 0:
             evaluate_file_path = dragged_file
-        else:
-            evaluate_file_path = self._script_path
 
-        the_file, new_filename = pathing_library_mojo(evaluate_file_path)
+        the_file, new_filename = self.library_pathing(evaluate_file_path)
 
         hyper_file = the_file
         new_export = new_filename
 
         return hyper_file, new_export
 
-    def hyper_to_csb(self, hyper_file, csv_export):
+    def hyper_to_csv(self, hyper_file, csv_export):
         """ Conversion from tableauhyperapi table "Dictionary" to a Pandas Dataframe
             Parameters:
                 hyper_file:
@@ -119,105 +120,6 @@ class HyperConvert:
         return df.to_csv(csv_export, index=None)
 
 
-
-
-
-def script_path():
-    # a decorator is possible here - for future refactoring
-    """ Edit these paths accordingly - this can be used for debugging and testing purposes.
-        Rather than setting this as a global variable, this is set as a local variable for further modular uses
-    """
-    SCRIPT_FILE_PATH = r'REDACTED FILE PATH'
-    return SCRIPT_FILE_PATH
-
-
-def pathing_library_mojo(some_file, file_convert='csv'):
-    """ Parse the file string and extract all necessawry elements to breakdown and conert a file to a desired format name.
-        Note: This function's purposes is only taking an input file and return the original path and newly converted file path
-        Parameters:
-            OPTIONAL File_convert
-                csv is the default file extension. For any other file extensions, just add what is needed
-                Note:
-                    Do not add a "." to any desired file extension when using this function
-        Returns:
-            orig_file_path:
-                The direct file path like "c:"
-            new_export:
-                This is the new file name - default is CSV
-        You need to import two standard libraries: pathlib and platform
-            pathlib is the bread and butter of the function
-            platform is just to check the operating system and perform a simple conversion
-    """
-    orig_file_path = pl.Path(some_file)
-    parent_path = str(orig_file_path.parent)
-    new_filename = orig_file_path.stem + '.' + file_convert
-
-    directory_pathing = '/'
-    if pf.system().lower() == 'windows':
-        directory_pathing = '\\'
-
-    new_export = parent_path + directory_pathing + new_filename
-
-    return orig_file_path, new_export
-
-
-def path_choice():
-    """ Output the files for conversion uses
-    
-        Returns:
-            hyper_file:
-                Tableau Dataset path
-            new_export:
-                Export dataset path
-    """
-    monologue()
-    while True:
-        try:
-            dragged_file = input("Press enter to use the file paths hard saved in this script\n> ")
-            break
-        except:
-            print("Unfortunately, an error occured. Perhaps you have quote marks. Delete those!")
-            continue
-    if len(dragged_file) != 0:
-        evaluate_file_path = dragged_file
-    else:
-        evaluate_file_path = script_path()
-
-    the_file, new_filename = pathing_library_mojo(evaluate_file_path)
-
-    hyper_file = the_file
-    new_export = new_filename
-
-    return hyper_file, new_export
-
-
-def hyper_to_csv(hyper_file, csv_export):
-    """ Conversion from tableauhyperapi table "Dictionary" to a Pandas Dataframe
-        Parameters: 
-            hyper_file:
-                File path for the hyper file import
-            csv_export:
-                File path for the csv file export
-        Steps:
-            1. Load the hyper tables in aggregate via pantab's "frames" from hyper to bypass the need for knowing individual table names
-            2. Run the dictionary items() and save the dataframe as a Pandas object
-        
-        Returns:
-            df.to_csv(csv_export, index=None) 
-                THe expected output to file path with no index, feel free to adjust as needed
-    """
-    hyperdb = pt.frames_from_hyper(hyper_file)
-    for key, pandas_df in hyperdb.items():
-        df = pandas_df
-    return df.to_csv(csv_export, index=None)
-
-
-def monologue():
-    print("You can choose to drag and drop a file path here!")
-    print("Otherwise, if the paths are hard coded into this script, ")
-    print("then do not type anything and simply just press enter")
-
-
 if __name__ == "__main__":
-    hyper_file, csv_export = path_choice()
-    hyper_to_csv(hyper_file, csv_export)
+    convert = HyperConvert()
+    convert.hyper_to_csv(convert.path_choice())
